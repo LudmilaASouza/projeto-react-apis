190e248bb0acd6d8301cfd1e9d335d9bc9c3aa34
LudmilaASouza <ludmilasouz.a@hotmail.com>
2023-07-07 23:13:46 -0300

    feat: integration API

--- src/Components/PokemonCard/PokemonCard.js
+++ src/Components/PokemonCard/PokemonCard.js
@@ -1,14 +1,13 @@
 import { ButtonContainer, ImgContainer, PokeCardContainer, PokeImg } from "./styled";
-import pikachu from "../../assets/pikachu.png"
 import { useNavigate } from "react-router-dom";
 import { goToDetailPage } from "../../Router/coordinator";
 
-const PokemonCard = () => {
+const PokemonCard = ({poke}) => {
     const navigate = useNavigate();
     return (        
         <PokeCardContainer>
             <ImgContainer>
-                <PokeImg src={pikachu} alt="Pikachu"/>
+                <PokeImg src={poke.sprites.front_default} alt={`Imagem do Pokemon ${poke.name}`}/>
             </ImgContainer>
             <ButtonContainer>
                 <button> Adicionar </button>
--- src/Pages/PokemonsListPage/PokemonsListPage.js
+++ src/Pages/PokemonsListPage/PokemonsListPage.js
@@ -1,14 +1,25 @@
+import { useEffect, useState } from "react";
 import PokemonCard from "../../Components/PokemonCard/PokemonCard";
 import {PokemonListContainer} from "./styled"
+import { getAllPokemons } from "../../API/request";
 
 const PokemonsListPage = () => {
+    const [pokemons, setPokemons] = useState([]);
+    useEffect(() => {
+        getAllPokemons(setPokemons)
+    }, [])
+    
+
+
     return (
         <PokemonListContainer>
-            <PokemonCard/>
-            <PokemonCard/>
-            <PokemonCard/>
-            <PokemonCard/>
-            <PokemonCard/>
+            {
+                pokemons.map((poke) => {
+                    return (
+                        <PokemonCard key={poke.id} poke={poke}/>
+                    )
+                })
+            }
         </PokemonListContainer>
     );
 };
